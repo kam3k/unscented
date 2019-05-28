dcc8339edbaba03d0bea28ad72c0f99dfe0b4168
Marc Gallant <marc.j.gallant@gmail.com>
2019-05-27 20:50:36 -0400

    Added skeleton of unit tests.

---
+++ test/test_unscented.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "unscented.hpp"
+
+namespace unscented
+{
+  TEST_CASE("Temp")
+  {
+    UKF<Eigen::Vector3d, 3, Eigen::Vector2d, 2> ukf;
+  }
+} // namespace unscented
